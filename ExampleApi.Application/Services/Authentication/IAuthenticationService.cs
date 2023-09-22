f0d480a67fdf7e1d80fa2e1f55c4581b60042b4f
grantshaw <grantshaw@hotmail.co.uk>
2023-09-23 00:07:19 +0100

    Auth service results.

---
+++ ExampleApi.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+namespace ExampleApi.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    AuthenticationResult Login(string firstName, string lastName, string email, string password);
+
+    AuthenticationResult Register(string email, string password);
+}
